177eb242ef45cf72528546ad7321a5eae2e22b9c
Bhautik Bhalala <47077595+Bhautik-Bhalala@users.noreply.github.com>
2022-11-12 16:29:05 +0530

    complete

---
+++ src/config/data.js
@@ -0,0 +1,34 @@
+export const chartDays = [
+  {
+    label: "24 Hours",
+    value: 1,
+  },
+  {
+    label: "7 days",
+    value: 7,
+  },
+  {
+    label: "30 Days",
+    value: 30,
+  },
+  {
+    label: "3 Months",
+    value: 90,
+  },
+  {
+    label: "6 months",
+    value: 180,
+  },
+  {
+    label: "1 Year",
+    value: 365,
+  },
+  {
+    label: "5 Year",
+    value: 1825,
+  },
+  {
+    label: "10 Years",
+    value: 3650,
+  },
+];
